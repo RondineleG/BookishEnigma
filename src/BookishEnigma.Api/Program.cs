395ef89f8a8c31a5bb71a7114624a8f5d2e03fc4
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-16 08:38:13 -0300

    Extension Methods and CORS Configuration

---
+++ src/BookishEnigma.Api/Program.cs
@@ -0,0 +1,21 @@
+﻿using BookishEnigma.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
