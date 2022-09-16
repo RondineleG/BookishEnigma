7175334af472e1cdb5b28ebe0123501237332ed2
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-16 09:16:19 -0300

    Creating Models

--- src/BookishEnigma.Api/Controllers/OwerController.cs
+++ src/BookishEnigma.Api/Controllers/OwerController.cs
@@ -1,28 +1,27 @@
 ﻿using BookishEnigma.Core.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookishEnigma.Api.Controllers
+namespace BookishEnigma.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OwerController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OwerController : ControllerBase
+    private readonly ILoggerManager _logger;
+
+    public OwerController(ILoggerManager logger)
     {
-        private readonly ILoggerManager _logger;
-
-        public OwerController(ILoggerManager logger)
-        {
-            _logger = logger;
-        }
+        _logger = logger;
+    }
 
 
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            _logger.LogInfo("Here is info message from the controller.");
-            _logger.LogDebug("Here is debug message from the controller.");
-            _logger.LogWarn("Here is warn message from the controller.");
-            _logger.LogError("Here is error message from the controller.");
-            return new string[] { "value1", "value2" };
-        }
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        _logger.LogInfo("Here is info message from the controller.");
+        _logger.LogDebug("Here is debug message from the controller.");
+        _logger.LogWarn("Here is warn message from the controller.");
+        _logger.LogError("Here is error message from the controller.");
+        return new string[] { "value1", "value2" };
     }
 }
--- src/BookishEnigma.Core/Contracts/ILoggerManager.cs
+++ src/BookishEnigma.Core/Contracts/ILoggerManager.cs
@@ -1,10 +1,9 @@
-﻿namespace BookishEnigma.Core.Contracts
+﻿namespace BookishEnigma.Core.Contracts;
+
+public interface ILoggerManager
 {
-    public interface ILoggerManager
-    {
-        void LogInfo(string message);
-        void LogWarn(string message);
-        void LogDebug(string message);
-        void LogError(string message);
-    }
+    void LogInfo(string message);
+    void LogWarn(string message);
+    void LogDebug(string message);
+    void LogError(string message);
 }
